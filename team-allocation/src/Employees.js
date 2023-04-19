5d5ba22b407c3ccfc4cb02d5d04e31ed49609f08
Tyler <td20147@gmail.com>
2023-04-18 21:10:51 -0400

    added employee team selection functionality

--- team-allocation/src/Employees.js
+++ team-allocation/src/Employees.js
@@ -91,22 +91,42 @@ const Employees = () => {
         teamName: "TeamD"
       }]);
 
+    const handleTeamSelectionChange = (event) => {
+        setSelectedTeam(event.target.value);
+    }
+
+    const handleEmployeeCardClick = (event) => {
+        const transformedEmployees = employees.map((employee) => {
+            if (employee.id === parseFloat(event.currentTarget.id)) {
+              if (employee.teamName === selectedTeam) {
+                return { ...employee, teamName: "" };
+              } else {
+                return { ...employee, teamName: selectedTeam };
+              }
+            } else {
+              return employee;
+            }
+          });
+          
+        setEmployees(transformedEmployees);
+    }
+
     return (
         <main className='container'>
-            <div class='row justify-content-center mt-3 mb-3'>
-                <div class="col-6">
-                    <select className="form-select form-select-lg">
+            <div className='row justify-content-center mt-3 mb-3'>
+                <div className="col-6">
+                    <select className="form-select form-select-lg" value={selectedTeam} onChange={handleTeamSelectionChange}>
                         <option value="TeamA">TeamA</option>
                         <option value="TeamB">TeamB</option>
                         <option value="TeamC">TeamC</option>
                         <option value="TeamD">TeamD</option>
                     </select>
                 </div>
-                <div class='col-8'>
-                    <div class="card-collection">
+                <div className='col-8'>
+                    <div className="card-collection">
                         {
                             employees.map((employee) => (
-                                <div id={employee.id} className='card m-2' style={{ cursor: "pointer" }}>
+                                <div id={employee.id} className={(employee.teamName === selectedTeam ? "card m-2 standout" : "card m-2")} style={{ cursor: "pointer" }} onClick={handleEmployeeCardClick}>
 
                                     {(employee.gender) === "male" ? <img src={maleProfile} className='card-img-top'/> : <img src={femaleProfile} className='card-img-top'/>}
                                     <div>
