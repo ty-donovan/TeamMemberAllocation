250b2ae20a9ff9aa545c0c27155d2bdd538342de
Tyler <td20147@gmail.com>
2023-04-18 20:46:40 -0400

    basic layout of employees

--- team-allocation/src/Employees.js
+++ team-allocation/src/Employees.js
@@ -1,7 +1,10 @@
 import { useState, useEffect } from 'react';
+import femaleProfile from "./images/femaleProfile.jpg";
+import maleProfile from "./images/maleProfile.jpg";
 
 
 const Employees = () => {
+    const [selectedTeam, setSelectedTeam] = useState("TeamB");
 
     const [employees, setEmployees] = useState([{
         id: 1,
@@ -89,8 +92,33 @@ const Employees = () => {
       }]);
 
     return (
-        <main>
+        <main className='container'>
+            <div class='row justify-content-center mt-3 mb-3'>
+                <div class="col-6">
+                    <select className="form-select form-select-lg">
+                        <option value="TeamA">TeamA</option>
+                        <option value="TeamB">TeamB</option>
+                        <option value="TeamC">TeamC</option>
+                        <option value="TeamD">TeamD</option>
+                    </select>
+                </div>
+                <div class='col-8'>
+                    <div class="card-collection">
+                        {
+                            employees.map((employee) => (
+                                <div id={employee.id} className='card m-2' style={{ cursor: "pointer" }}>
 
+                                    {(employee.gender) === "male" ? <img src={maleProfile} className='card-img-top'/> : <img src={femaleProfile} className='card-img-top'/>}
+                                    <div>
+                                        <h5 className='card-title'>Full Name: {employee.fullName}</h5>
+                                        <p className='card-text'><b>Designation:</b> {employee.designation}</p>
+                                    </div>
+                                </div>
+                            ))
+                        }
+                    </div>
+                </div>
+            </div>
         </main>
     )
 }
