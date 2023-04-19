ba931f6c863f1eb807ba036eda8b9d690bdca27d
Tyler <td20147@gmail.com>
2023-04-19 16:48:09 -0400

    put employees functionality into app.js so data can be passed down to other components

--- team-allocation/src/App.js
+++ team-allocation/src/App.js
@@ -4,14 +4,125 @@ import Header from './Header';
 import Content from './Content';
 import Footer from './Footer';
 import Employees from './Employees';
+import { useState } from 'react';
 
 function App() {
+  const [selectedTeam, setSelectedTeam] = useState("TeamB");
+
+  const [employees, setEmployees] = useState([{
+      id: 1,
+      fullName: "Bob Jones",
+      designation: "JavaScript Developer",
+      gender: "male",
+      teamName: "TeamA"
+    },
+    {
+      id: 2,
+      fullName: "Jill Bailey",
+      designation: "Node Developer",
+      gender: "female",
+      teamName: "TeamA"
+    },
+    {
+      id: 3,
+      fullName: "Gail Shepherd",
+      designation: "Java Developer",
+      gender: "female",
+      teamName: "TeamA"
+    },
+    {
+      id: 4,
+      fullName: "Sam Reynolds",
+      designation: "React Developer",
+      gender: "male",
+      teamName: "TeamB"
+    },
+    {
+      id: 5,
+      fullName: "David Henry",
+      designation: "DotNet Developer",
+      gender: "male",
+      teamName: "TeamB"
+    },
+    {
+      id: 6,
+      fullName: "Sarah Blake",
+      designation: "SQL Server DBA",
+      gender: "female",
+      teamName: "TeamB"
+    },
+    {
+      id: 7,
+      fullName: "James Bennet",
+      designation: "Angular Developer",
+      gender: "male",
+      teamName: "TeamC"
+    },
+    {
+      id: 8,
+      fullName: "Jessica Faye",
+      designation: "API Developer",
+      gender: "female",
+      teamName: "TeamC"
+    },
+    {
+      id: 9,
+      fullName: "Lita Stone",
+      designation: "C++ Developer",
+      gender: "female",
+      teamName: "TeamC"
+    },
+    {
+      id: 10,
+      fullName: "Daniel Young",
+      designation: "Python Developer",
+      gender: "male",
+      teamName: "TeamD"
+    },
+    {
+      id: 11,
+      fullName: "Adrian Jacobs",
+      designation: "Vue Developer",
+      gender: "male",
+      teamName: "TeamD"
+    },
+    {
+      id: 12,
+      fullName: "Devin Monroe",
+      designation: "Graphic Designer",
+      gender: "male",
+      teamName: "TeamD"
+    }]);
+
+  const handleTeamSelectionChange = (event) => {
+      setSelectedTeam(event.target.value);
+  }
+
+  const handleEmployeeCardClick = (event) => {
+      const transformedEmployees = employees.map((employee) => {
+          if (employee.id === parseFloat(event.currentTarget.id)) {
+            if (employee.teamName === selectedTeam) {
+              return { ...employee, teamName: "" };
+            } else {
+              return { ...employee, teamName: selectedTeam };
+            }
+          } else {
+            return employee;
+          }
+        });
+        
+      setEmployees(transformedEmployees);
+  }
+
   return (
     <div style={{ textAlign: 'center' }}>
       <Header />
       <Content />
       <Footer />
-      <Employees />
+      <Employees employees={employees}
+        selectedTeam={selectedTeam}
+        handleEmployeeCardClick={handleEmployeeCardClick}
+        handleTeamSelectionChange={handleTeamSelectionChange}/>
     </div>
   );
 }
--- team-allocation/src/Employees.js
+++ team-allocation/src/Employees.js
@@ -1,115 +1,8 @@
-import { useState, useEffect } from 'react';
 import femaleProfile from "./images/femaleProfile.jpg";
 import maleProfile from "./images/maleProfile.jpg";
 
 
-const Employees = () => {
-    const [selectedTeam, setSelectedTeam] = useState("TeamB");
-
-    const [employees, setEmployees] = useState([{
-        id: 1,
-        fullName: "Bob Jones",
-        designation: "JavaScript Developer",
-        gender: "male",
-        teamName: "TeamA"
-      },
-      {
-        id: 2,
-        fullName: "Jill Bailey",
-        designation: "Node Developer",
-        gender: "female",
-        teamName: "TeamA"
-      },
-      {
-        id: 3,
-        fullName: "Gail Shepherd",
-        designation: "Java Developer",
-        gender: "female",
-        teamName: "TeamA"
-      },
-      {
-        id: 4,
-        fullName: "Sam Reynolds",
-        designation: "React Developer",
-        gender: "male",
-        teamName: "TeamB"
-      },
-      {
-        id: 5,
-        fullName: "David Henry",
-        designation: "DotNet Developer",
-        gender: "male",
-        teamName: "TeamB"
-      },
-      {
-        id: 6,
-        fullName: "Sarah Blake",
-        designation: "SQL Server DBA",
-        gender: "female",
-        teamName: "TeamB"
-      },
-      {
-        id: 7,
-        fullName: "James Bennet",
-        designation: "Angular Developer",
-        gender: "male",
-        teamName: "TeamC"
-      },
-      {
-        id: 8,
-        fullName: "Jessica Faye",
-        designation: "API Developer",
-        gender: "female",
-        teamName: "TeamC"
-      },
-      {
-        id: 9,
-        fullName: "Lita Stone",
-        designation: "C++ Developer",
-        gender: "female",
-        teamName: "TeamC"
-      },
-      {
-        id: 10,
-        fullName: "Daniel Young",
-        designation: "Python Developer",
-        gender: "male",
-        teamName: "TeamD"
-      },
-      {
-        id: 11,
-        fullName: "Adrian Jacobs",
-        designation: "Vue Developer",
-        gender: "male",
-        teamName: "TeamD"
-      },
-      {
-        id: 12,
-        fullName: "Devin Monroe",
-        designation: "Graphic Designer",
-        gender: "male",
-        teamName: "TeamD"
-      }]);
-
-    const handleTeamSelectionChange = (event) => {
-        setSelectedTeam(event.target.value);
-    }
-
-    const handleEmployeeCardClick = (event) => {
-        const transformedEmployees = employees.map((employee) => {
-            if (employee.id === parseFloat(event.currentTarget.id)) {
-              if (employee.teamName === selectedTeam) {
-                return { ...employee, teamName: "" };
-              } else {
-                return { ...employee, teamName: selectedTeam };
-              }
-            } else {
-              return employee;
-            }
-          });
-          
-        setEmployees(transformedEmployees);
-    }
+const Employees = ({selectedTeam, handleEmployeeCardClick, handleTeamSelectionChange, employees}) => {
 
     return (
         <main className='container'>
@@ -128,7 +21,7 @@ const Employees = () => {
                             employees.map((employee) => (
                                 <div id={employee.id} className={(employee.teamName === selectedTeam ? "card m-2 standout" : "card m-2")} style={{ cursor: "pointer" }} onClick={handleEmployeeCardClick}>
 
-                                    {(employee.gender) === "male" ? <img src={maleProfile} className='card-img-top'/> : <img src={femaleProfile} className='card-img-top'/>}
+                                    {(employee.gender) === "male" ? <img src={maleProfile} className='card-img-top' alt="maleProfile"/> : <img src={femaleProfile} className='card-img-top' alt="femaleProfile"/>}
                                     <div>
                                         <h5 className='card-title'>Full Name: {employee.fullName}</h5>
                                         <p className='card-text'><b>Designation:</b> {employee.designation}</p>
